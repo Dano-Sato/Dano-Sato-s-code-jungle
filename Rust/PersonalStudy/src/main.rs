9e21c7b506c44acc0457839790f28d3690cec7d8
Dano Sato <ten0102@naver.com>
2020-03-11 01:26:01 +0900

    Rust_First_App

--- Rust/PersonalStudy/src/main.rs
+++ Rust/PersonalStudy/src/main.rs
@@ -7,7 +7,13 @@ fn main() {
         let secret_number = rand::thread_rng().gen_range(1,101); 
 
         println!("The secret number is: {}",secret_number);
+
+
+        //infinite loop starts at this point.
+        //You can get out of the loop by "break" keyword.
+
         loop{
+
         println!("Guess the number!");
         println!("Please input your guess.");
 
@@ -25,6 +31,10 @@ fn main() {
             Ok(num) =>num,
             Err(_) => continue,
         };
+        //Error handling with match expression.
+        //match expression could return value,and the value could be assigned to variable.
+    
+
 
         println!("You guessed: {}", guess);    
 
